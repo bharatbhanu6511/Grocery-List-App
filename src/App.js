e38454124a23c8f364357dd86a048a66519000a7
bharatbhanu6511 <pratapbhanu364@gmail.com>
2024-03-18 10:49:01 +0530

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
@@ -8,14 +8,32 @@ import AddItem from './AddItem';
 import SearchItem from './SearchItem';
 
 function App() {
-  const storedItems = JSON.parse(localStorage.getItem('shoping list')) || [];
-  const [items, setItems] = useState(storedItems);
+  const [items, setItems] = useState(JSON.parse(localStorage.getItem('shoping list')) || []);
+  // the '||' is called short circuit operator, even if you dont habe this [] after || the app would still run fine but only because when we remove all the items from list the shoping list in local storage sets to [] and this can happen only if there was already a shoping list in local storage, if the app was being run for the very first time there would be no shoping list in local storage and the console would return an error saying that null cannot be filtered 
   const [newItem, setNewItem] = useState('')
   const [search, setSearch] = useState('')
 
+  // console.log('before use effect')
+  // runs every time you type in box
+
+  // useEffect(()=>{
+  //   console.log('render');
+  // })
+  // logs 'render' to console every time page renders or when every time you type a new character in the 'add item' input box
+
+  useEffect(() => {
+    localStorage.setItem('shoping list', JSON.stringify(items))
+    // console.log('updating items state');
+    // runs asynchronously after 'after use effect' and only when items changes
+  }, [items])
+  // logs 'render' to the console only at the time of loading page, it does not logs everytime you type a new character in the 'add item' input box, the use effect looks in [] everytime since it is a dependency, if contents inside [] don't change it does not logs, it will log only when contents inside [] change
+
+  // console.log('after use effect')
+  // runs every time you type in box
+
   const setAndSaveItems = (newItems) => {
     setItems(newItems)
-    localStorage.setItem('shoping list', JSON.stringify(newItems))
+    
   }
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1
@@ -44,7 +62,13 @@ function App() {
       <Header title="Grocery Store" />
       <SearchItem search={search} setSearch={setSearch} />
       <AddItem newItem={newItem} setNewItem={setNewItem} handleSubmit={handleSubmit} />
-      <Content items={items.filter(item=>((item.item).toLowerCase()).includes(search.toLowerCase()))} setItems={setItems} handleCheck={handleCheck} handleDelete={handleDelete} />
+      <Content
+        items={
+          items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))
+          // returns error that null cannot be filtered when [] is not there after || in useState()
+        }
+        setItems={setItems} handleCheck={handleCheck} handleDelete={handleDelete}
+      />
       <Footer />
     </div>
   );
